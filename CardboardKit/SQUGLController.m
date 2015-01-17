2d0372e7c3f130ae3fb868314fcdaadf1a59cf22
Tristan Seifert <tristan@squee.co>
2015-01-17 13:47:32 -0500

    yolo

--- CardboardKit/SQUGLController.m
+++ CardboardKit/SQUGLController.m
@@ -36,7 +36,7 @@
 	if(self = [super initWithNibName:nil bundle:nil]) {
 		_renderisor = renderer;
 		
-		_offset = 0.75;
+		_offset = 0.2;
 		_initialised = NO;
 		
 		[[SQUCardboardKit sharedInstance] addObserver:self forKeyPath:@"cameraAngle" options:0 context:NULL];
@@ -123,7 +123,7 @@
 	
 	_renderViewLeft = [[SCNView alloc] initWithFrame:l];
 	_renderViewLeft.preferredFramesPerSecond = 50;
-	_renderViewLeft.antialiasingMode = SCNAntialiasingModeNone;
+	_renderViewLeft.antialiasingMode = SCNAntialiasingModeMultisampling2X;
 	_renderViewLeft.backgroundColor = self.view.backgroundColor;
 	_renderViewLeft.delegate = self;
 	_renderViewLeft.showsStatistics = YES;
